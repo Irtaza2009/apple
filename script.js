82bcd37b20b672b2b6d5ad9e9f2554476a5f8c8b
Irtaza <irtazanaqvi05@gmail.com>
2024-07-25 18:09:14 +0500

    added transitions

--- script.js
+++ script.js
@@ -6,3 +6,34 @@ document.addEventListener("scroll", function () {
     header.classList.remove("scrolled");
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const observerOptions = {
+    threshold: 0.1,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  const sections = document.querySelectorAll(
+    ".about-section, .projects-section, .contact-section, footer"
+  );
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+
+  const header = document.querySelector("header");
+  document.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+});
