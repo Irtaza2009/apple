8d2c83a74360021c7633661d0be51a4d6cd45d88
Irtaza <irtazanaqvi05@gmail.com>
2024-07-25 20:51:24 +0500

    atlast got the apple animation working

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const zoom = document.querySelector(".zoom");
+const zoom2 = document.querySelector(".zoom2");
+const zoom3 = document.querySelector(".zoom3");
 const minZoom = 1;
 const maxZoom = 2;
 
@@ -10,5 +12,29 @@ addEventListener("scroll", (e) => {
   if (scrollTop > start && scrollTop < stop) {
     const scale = Math.max(2.2 - (scrollTop - start) / 500, 1);
     zoom.style.transform = `scale(${scale})`;
+  }
+});
+
+addEventListener("scroll", (e) => {
+  const vh = window.innerHeight / 100;
+  const scrollTop = document.documentElement.scrollTop;
+  const start = 200 * vh;
+  const stop = 400 * vh;
+  if (scrollTop > start && scrollTop < stop) {
+    const scale = Math.max(2.2 - (scrollTop - start) / 500, 1);
+
+    zoom2.style.transform = `scale(${scale})`;
+  }
+});
+
+addEventListener("scroll", (e) => {
+  const vh = window.innerHeight / 100;
+  const scrollTop = document.documentElement.scrollTop;
+  const start = 300 * vh;
+  const stop = 600 * vh;
+  if (scrollTop > start && scrollTop < stop) {
+    const scale = Math.max(2.2 - (scrollTop - start) / 500, 1);
+
+    zoom3.style.transform = `scale(${scale})`;
   }
 });
