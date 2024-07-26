56e015e00b84e79ea63a530ee5f1844989ed6f44
Irtaza <irtazanaqvi05@gmail.com>
2024-07-26 19:14:09 +0500

    added animate on scroll animations to the projects carousel

--- script.js
+++ script.js
@@ -1,49 +1,6 @@
-/*const zoom = document.querySelector(".zoom");
-const zoom2 = document.querySelector(".zoom2");
-const zoom3 = document.querySelector(".zoom3");
-const minZoom = 1;
-const maxZoom = 2;
-
-addEventListener("scroll", (e) => {
-  const vh = window.innerHeight / 100;
-  const scrollTop = document.documentElement.scrollTop;
-  const start = 100 * vh;
-  const stop = 200 * vh;
-  if (scrollTop > start && scrollTop < stop) {
-    const scale = Math.max(2.2 - (scrollTop - start) / 500, 1);
-    zoom.style.transform = `scale(${scale})`;
-  }
-});
-
-addEventListener("scroll", (e) => {
-  const vh = window.innerHeight / 100;
-  const scrollTop = document.documentElement.scrollTop;
-  const start = 400 * vh;
-  const stop = 500 * vh;
-  if (scrollTop > start && scrollTop < stop) {
-    const scale = Math.max(2.2 - (scrollTop - start) / 500, 1);
-
-    zoom2.style.transform = `scale(${scale})`;
-  }
-});
-
-addEventListener("scroll", (e) => {
-  const vh = window.innerHeight / 100;
-  const scrollTop = document.documentElement.scrollTop;
-  const start = 700 * vh;
-  const stop = 800 * vh;
-  if (scrollTop > start && scrollTop < stop) {
-    const scale = Math.max(2.2 - (scrollTop - start) / 500, 1);
-
-    zoom3.style.transform = `scale(${scale})`;
-  }
-});
-*/
-
 document.addEventListener("DOMContentLoaded", () => {
   const zoomElements = document.querySelectorAll(".zoom, .zoom2, .zoom3");
   const zoomFactor = 2.2;
-  const transitionDuration = 500; // Duration of transition in milliseconds
   const vh = window.innerHeight / 100;
 
   function updateZoom(scrollTop) {
@@ -63,14 +20,18 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   window.addEventListener("scroll", handleScroll);
-});
 
-document.addEventListener("DOMContentLoaded", function () {
+  // Initialize AOS
+  AOS.init({
+    duration: 500, // Animation duration in milliseconds
+    mirror: true, // whether elements should animate out while scrolling past them
+  });
+
   const cards = document.querySelectorAll(".project-card");
   const options = {
     root: null,
     threshold: 0.5,
-    rootMargin: "-10% 0px", // Adjust as needed
+    rootMargin: "10px 0px -90% 0px", // Adjust as needed to stick 10px from top
   };
 
   const observer = new IntersectionObserver((entries) => {
